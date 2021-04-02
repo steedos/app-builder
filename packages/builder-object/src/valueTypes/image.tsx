0a1d4aba915b8aa87406b62d06112940f79da873
廖大雪 <2291335922@qq.com>
2021-04-02 18:30:46 +0800

    imgae组件增加部分图片预览效果等

--- packages/builder-object/src/valueTypes/image.tsx
+++ packages/builder-object/src/valueTypes/image.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {Input} from 'antd';
 import { Upload, Button, message } from 'antd';
 import { UploadOutlined } from '@ant-design/icons';
-
+import { Image } from "antd";
 const { STEEDOS_ROOT_URL } = process.env
 
 /*
@@ -11,68 +11,32 @@ const { STEEDOS_ROOT_URL } = process.env
  * 组件支持属性
  * - id : string
  * - name: string
+ * 比如华炎魔方space_users对象中有一个avatar字段
+ *  avatar:
+ *    type: "avatar"
+ *    label: "头像"
+ *    name: "avatra"
+ * 对应的组件属性具体为
+ * - type: "image"
+ * - multiple: true
  * 
  * 保存到spa_lfa1表记录中对应为以下效果
- *    "id": "pcsscrbDSMXDBQjbA"
  *    "imgs__c": "iHXH3SrYP5ZcarQbX"
- * 
- * 对应的组件属性具体为
- * - name: "imgs__c"
- * - url: string
 */
 
 export const image = {
     render: (text : any, props : any) => {
-        console.log('图片进入readonly状态')
+        // console.log('图片进入readonly状态',props)
         let url = STEEDOS_ROOT_URL+'/api/files/images/'+text ;
         url= url ? url : 'https://avatars.githubusercontent.com/u/3104826?s=200&v=4';
-        console.log(url)
-        return (<img title="img" style={{ height: "50px"}} src={url}/>)
+        return (<Image alt="图片" width={50} src={url} />)
     },
     renderFormItem: (_ : any, props : any) => {
-      console.log('renderFormItem编辑状态', props)
-        // return (<Input>image edit</div>)
-        // return (<Input placeholder="请输入哈哈哈" {...props}/>)
-        // return (<imageupdate>defaultInsertRecord</imageupdate>)
-        // const props = {
-        //   beforeUpload: file => {
-        //     if (file.type !== 'image/png') {
-        //       message.error(`${file.name} is not a png file`);
-        //     }
-        //     return file.type === 'image/png' ? true : Upload.LIST_IGNORE;
-        //   },
-        //   onChange: info => {
-        //     console.log(info.fileList);
-        //   },
-        // };
-
-        
+        // console.log('图片进入edit状态',props)
         return (
-          <Upload {...props} type="file" accept="image/png, image/jpeg, image/gif" action="" maxCount="1" > 
+          <Upload {...props} type="file" method="post" accept="image/png, image/jpeg, image/gif" action="http://192.168.0.98:5066/s3" maxCount="1"> 
             <Button icon={<UploadOutlined />} title="上传图片">Upload png only</Button>
           </Upload>
         );
     }
 }
-      //  export const isImageUrl = (file: UploadFile): boolean => {
-      //   if (file.type) {
-      //     return isImageFileType(file.type);
-      //   }
-      //   const url: string = (file.thumbUrl || file.url) as string;
-      //   const extension = extname(url);
-      //   if (
-      //     /^data:image\//.test(url) ||
-      //     /(webp|svg|png|gif|jpg|jpeg|jfif|bmp|dpg|ico)$/i.test(extension)
-      //   ) {
-      //     return true;
-      //   }
-      //   if (/^data:/.test(url)) {
-      //     // other file types of base64
-      //     return false;
-      //   }
-      //   if (extension) {
-      //     // other file types which have extension
-      //     return false;
-      //   }
-      //   return true;
-      // };
