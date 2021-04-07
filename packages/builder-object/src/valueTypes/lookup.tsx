38b4a249cf5a94d1158afa5b8dd42eec54d28f26
殷亮辉 <yinlianghui@hotoa.com>
2021-04-07 15:36:33 +0800

    fix lookup read mode

--- packages/builder-object/src/valueTypes/lookup.tsx
+++ packages/builder-object/src/valueTypes/lookup.tsx
@@ -1,4 +1,5 @@
 import React, { useContext, useEffect, useState } from "react";
+import { useQuery } from 'react-query'
 
 import { Tag } from 'antd';
 
@@ -9,16 +10,44 @@ import { ObjectContext } from "../providers/ObjectContext";
 import FieldSelect, {
     proFieldParsingText,
     proFieldParsingValueEnumToArray,
-  } from '@ant-design/pro-field/es/components/Select';
+} from '@ant-design/pro-field/es/components/Select';
 
 // 相关表类型字段
 // 通过下拉框显示相关表中的数据，可以搜索
 // 参数 props.reference_to:
 
-const renderFormItem = (_: any, props: any, formMode) => {
-        
+const render = (text: any, props: any) => {
+    // console.log("===render====props==", props);
     const objectContext = useContext(ObjectContext);
-    const { fieldSchema={}, mode, valueType, fieldProps, ...rest } = props;
+    const { fieldSchema = {}, valueType, fieldProps, ...rest } = props;
+    const { reference_to, multiple } = fieldSchema;
+    const value = fieldProps.value;
+    const [label, setLabel] = useState(value);
+    const href = `/app/-/${reference_to}/view/${value}`
+
+    const filter = value ? ['_id', '=', value] : [];
+    const fields = ['_id', 'name'];
+
+    const recordsQuery = useQuery<any>([reference_to, filter, fields], async () => {
+        const records = await objectContext.requestRecords(reference_to, filter, fields);
+        if (records && records.value && records.value.length > 0) {
+            const record = records.value[0];
+            setLabel(record.name);
+        }
+    },
+        {
+            enabled: !!value
+        }
+    );
+
+    if (!recordsQuery.isSuccess) return (<div>Loading record ...</div>)
+    return (<a href={href}>{label}</a>)
+}
+
+
+const renderFormItem = (_: any, props: any, formMode: any) => {
+    const objectContext = useContext(ObjectContext);
+    const { fieldSchema = {}, mode, valueType, fieldProps, ...rest } = props;
     const { reference_to, multiple } = fieldSchema;
     if (multiple)
         fieldProps.mode = 'multiple';
@@ -26,7 +55,6 @@ const renderFormItem = (_: any, props: any, formMode) => {
     const tagRender = (props) => {
         const { label, value, closable, onClose } = props;
         const href = `/app/-/${reference_to}/view/${value}`
-        
         return (
             <Tag closable={closable} onClose={onClose} style={{ marginRight: 3 }}>
                 {formMode === 'read' && (<a href={href}>{label}</a>)}
@@ -36,25 +64,24 @@ const renderFormItem = (_: any, props: any, formMode) => {
     }
 
     // 注意，request 里面的代码不会抛异常，包括编译错误。
-    const request = async (params, props) => {
-        
+    const request = async (params: any, props: any) => {
         let filters = [];
         if (params.keyWords && props.text)
             filters = [['name', 'contains', params.keyWords], 'or', ['_id', '=', props.text]]
-        else if ( props.text)
+        else if (props.text)
             filters = [['_id', '=', props.text]]
         else if (params.keyWords)
             filters = [['name', 'contains', params.keyWords]]
         const fields = ['_id', 'name'];
         const data = await objectContext.requestRecords(reference_to, filters, fields);
 
-        const options = data.value.map( (item)=> {
+        const options = data.value.map((item) => {
             return {
                 label: item.name,
                 value: item._id
             }
         })
-        
+
         return options
     }
 
@@ -74,9 +101,10 @@ const renderFormItem = (_: any, props: any, formMode) => {
     )
 }
 
+
 export const lookup = {
     render: (text: any, props: any) => {
-        return renderFormItem(text, props, 'read')
+        return render(text, props)
     },
     renderFormItem: (text: any, props: any) => {
         return renderFormItem(text, props, 'edit')
