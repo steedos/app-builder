1469bb7837f084ec3798518ec228e71861a944dd
baozhoutao <baozhoutao@hotoa.com>
2021-04-19 17:10:40 +0800

    add @steedos/react-webapp

---
+++ packages/react-webapp/src/App.tsx
@@ -0,0 +1,64 @@
+import React, { useRef } from 'react';
+import ProLayout, { PageContainer } from '@ant-design/pro-layout';
+import { Button } from 'antd';
+import { SteedosProvider } from '@steedos/builder-steedos';
+import { ObjectTable } from '@steedos/builder-object';
+import { API } from '@steedos/builder-store';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import Layout from './layouts';
+
+const STEEDOS_ROOT_URL = "http://192.168.3.2:5080"
+const STEEDOS_TENANT_ID = "cei48XfStzib6SHER"
+const STEEDOS_USER_ID = "604189e68753dfc2885e6226"
+const STEEDOS_AUTH_TOKEN = "12f86909afc6ff1c0aa1ec6f49b1d4275a5c08595df3c077d5d3afb39e7d55d8d862cf5846a6378e7c67d1"
+
+const initialStore = {
+  rootUrl: STEEDOS_ROOT_URL,
+  tenantId: STEEDOS_TENANT_ID,
+  userId: STEEDOS_USER_ID,
+  authToken: STEEDOS_AUTH_TOKEN,
+  currentObjectApiName: 'accounts',
+  locale: 'zh_CN',
+  forms: {},
+  objects: {},
+}
+
+const routes = [
+  {
+    path: "/app/:appApiName",
+    component: Layout
+  }
+];
+
+function RouteWithSubRoutes(route: any) {
+  return (
+    <Route
+      path={route.path}
+      render={props => {
+        // pass the sub-routes down to keep nesting
+        const appApiName = props.match.params.appApiName
+        return <route.component {...props} appApiName={appApiName} routes={route.routes} />
+      }}
+    />
+  );
+}
+
+export default function App() {
+  return (
+    <SteedosProvider {...initialStore}>
+      <Router>
+        <Switch>
+          {routes.map((route, i) => (
+            <RouteWithSubRoutes key={i} {...route} />
+          ))}
+        </Switch>
+      </Router>
+    </SteedosProvider>
+  );
+};
---
+++ packages/react-webapp/src/layouts/index.tsx
@@ -0,0 +1,76 @@
+import React, { useRef, useState } from 'react';
+import ProLayout, { PageContainer } from '@ant-design/pro-layout';
+import { ObjectTable } from '@steedos/builder-object';
+import { API } from '@steedos/builder-store';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+
+const routes = [
+    {
+      path: "/app/:appApiName/:objectApiName",
+      component: ObjectTable
+    }
+  ];
+  
+  function RouteWithSubRoutes(route: any) {
+    return (
+      <Route
+        path={route.path}
+        render={props => {
+          // pass the sub-routes down to keep nesting
+          const objectApiName = props.match.params.objectApiName
+          return <route.component {...props} objectApiName={objectApiName} columnFields={[{ fieldName: "name" }]} routes={route.routes} />
+        }}
+      />
+    );
+  }
+
+export default function Layout(props: any) {
+    const { appApiName } = props;
+    const [appName, setAppName] = useState('');
+    const [appIcon, setAppIcon] = useState(API.client.getUrl() + '/packages/steedos_creator/assets/logo-square.png');
+    return (
+          <ProLayout
+            title={appName}
+            // logo = {appIcon}
+            style={{
+              // height: '100vh',
+              // border: '1px solid #ddd',
+            }}
+            navTheme='light'
+            menuItemRender={(item, dom) => {
+              if (item.path?.startsWith('http://') || item.path?.startsWith('https://')) {
+                return <a target='_blank' href={item.path}>{item.name}</a>
+              } else {
+                return <Link to={item.path || '/welcome'}>
+                  {item.name}
+                </Link>
+              }
+  
+            }}
+            menu={{
+              request: async () => {
+                const appMenus: any = await API.client.doFetch(API.client.getUrl() + `/service/api/apps/${appApiName}/menus`, { method: 'get' });
+                setAppName(appMenus.name);
+                return appMenus.children
+              },
+            }}
+            location={{
+              pathname: '/welcome/welcome',
+            }}
+          >
+            <PageContainer content={false} title={false} header={undefined}>
+              <Switch>
+                {routes.map((route, i) => (
+                  <RouteWithSubRoutes key={i} {...route} />
+                ))}
+              </Switch>
+            </PageContainer>
+          </ProLayout>
+    );
+  };
