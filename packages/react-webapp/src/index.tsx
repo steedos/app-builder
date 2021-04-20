eec432b9ccdf2731fac07ce0e660fca9112454ae
Jack Zhuang <50353452+hotlong@users.noreply.github.com>
2021-04-20 18:42:37 +0800

    css

--- packages/react-webapp/src/index.tsx
+++ packages/react-webapp/src/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'antd/dist/antd.css';
-import './index.css';
+import '@ant-design/pro-layout/dist/layout.css'
+import '@ant-design/pro-table/dist/table.css'
+import '@ant-design/pro-form/dist/form.css'
+
 import App from './App';
 ReactDOM.render(
   <App />,
