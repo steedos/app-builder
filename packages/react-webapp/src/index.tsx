57c41090b9c42f4c2eb0514337020c63840dc4cd
Jack Zhuang <50353452+hotlong@users.noreply.github.com>
2021-04-20 18:52:03 +0800

    use less

--- packages/react-webapp/src/index.tsx
+++ packages/react-webapp/src/index.tsx
@@ -1,9 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'antd/dist/antd.css';
-import '@ant-design/pro-layout/dist/layout.css'
-import '@ant-design/pro-table/dist/table.css'
-import '@ant-design/pro-form/dist/form.css'
 
 import App from './App';
 ReactDOM.render(
