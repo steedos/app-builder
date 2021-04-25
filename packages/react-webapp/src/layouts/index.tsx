0bf02b3550bef829ee847a791eccb8b06c76269b
廖大雪 <2291335922@qq.com>
2021-04-25 19:43:41 +0800

    还原无logo

--- packages/react-webapp/src/layouts/index.tsx
+++ packages/react-webapp/src/layouts/index.tsx
@@ -109,7 +109,7 @@ export const Layout = observer((props: any) => {
         const {collapsed, logo: logoUrl, title: titleValue} = menuProps;
         return (
           <div>
-            {collapsed ? null : (logoUrl ? (<img src={logoUrl.toString()} alt={titleValue.toString()} />) : null)}
+            {/* {collapsed ? null : (logoUrl ? (<img src={logoUrl.toString()} alt={titleValue.toString()} />) : null)} */}
             <SteedosAppLauncher currentApp={currentApp} apps={apps} history={history}/>
           </div>
       ) }}
