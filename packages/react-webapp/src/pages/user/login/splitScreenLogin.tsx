7816e6f61e21140d3ea6e4bd282caf8ef4616316
殷亮辉 <yinlianghui@hotoa.com>
2021-04-26 15:40:19 +0800

    登录界面页面标题改为Steedos #20

--- packages/react-webapp/src/pages/user/login/splitScreenLogin.tsx
+++ packages/react-webapp/src/pages/user/login/splitScreenLogin.tsx
@@ -31,6 +31,7 @@ export const SplitScreenLogin = observer((props: any) => {
         return false;
     };
 
+    document.title = "Steedos";
     return (
         <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
             <div className="sm:mx-auto sm:w-full sm:max-w-md">
