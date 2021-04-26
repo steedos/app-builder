bbd570c1b9b9cde57a181b103660871fa8e1b40b
廖大雪 <2291335922@qq.com>
2021-04-26 13:27:35 +0800

    logo 代码优化

--- packages/react-webapp/src/layouts/index.tsx
+++ packages/react-webapp/src/layouts/index.tsx
@@ -126,9 +126,7 @@ export const Layout = observer((props: any) => {
         const {collapsed, logo: logoUrl, title: titleValue} = menuProps || {};
         return (
           <div>
-            <div className="steedos-logo">
-              {collapsed ? null : (logoUrl ? (<img src={logoUrl.toString()} alt={titleValue.toString()} />) : null)}
-            </div>
+            {collapsed ? null : (logoUrl ? (<div className="steedos-logo"><img src={logoUrl.toString()} alt={titleValue.toString()} /></div>) : null)}
             <SteedosAppLauncher currentApp={currentApp} apps={apps} history={history}/>
           </div>
       ) }}
