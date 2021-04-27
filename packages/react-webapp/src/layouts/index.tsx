d63994540d379d038c957320cac19b0903468472
廖大雪 <2291335922@qq.com>
2021-04-27 10:59:58 +0800

    优化变量名

--- packages/react-webapp/src/layouts/index.tsx
+++ packages/react-webapp/src/layouts/index.tsx
@@ -130,9 +130,9 @@ export const Layout = observer((props: any) => {
       logo={logoAvatarUrl}
       menuHeaderRender={(logo, title, menuProps) => { 
         const {collapsed, logo: logoUrl, title: titleValue} = menuProps || {};
-        const logoeExtra = logoUrl ? (<img src={logoUrl.toString()} alt={titleValue.toString()} />) :null;
+        const logoDom = logoUrl ? (<img src={logoUrl.toString()} alt={titleValue.toString()} />) :null;
         return (
-          logoeExtra
+          logoDom
       ) }}
       menuExtraRender={() => (<SteedosAppLauncher currentApp={currentApp} apps={apps} history={history}/>)}
       fixSiderbar={true}
