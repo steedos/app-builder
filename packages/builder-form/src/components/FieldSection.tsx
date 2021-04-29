3922a720c0696e586ffe2fb73f08e89647431fbd
Jack Zhuang <50353452+hotlong@users.noreply.github.com>
2021-04-29 11:38:50 +0800

    Update FieldSection.tsx

--- packages/builder-form/src/components/FieldSection.tsx
+++ packages/builder-form/src/components/FieldSection.tsx
@@ -1,16 +1,20 @@
 import React, { useContext } from 'react';
 import { Grid, GridItem, Flex, Box } from '@chakra-ui/layout'
 import { observer } from "mobx-react-lite"
+import useAntdMediaQuery from 'use-media-antd-query';
 
 import ExpandableSection from '@salesforce/design-system-react/components/expandable-section';
 import _ from 'lodash';
 
 export const FieldSection = observer((props: any) => {
+
+  const colSize = useAntdMediaQuery();
+
   const { 
     attributes, // Builder.io 传过来的参数。
     title, 
     titleHidden = false,
-    columns = 2, 
+    columns = (colSize === 'sm' || colSize === 'xs')? 1: (colSize === 'xl')? 3: (colSize === 'xxl')? 4: 2, 
     children 
   } = props
   
