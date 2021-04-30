0589c104e19c9d4b5bd4909d199e216c88989c0c
Jack Zhuang <50353452+hotlong@users.noreply.github.com>
2021-05-01 01:53:32 +0800

    ag grid

--- packages/builder-object/src/valueTypes/aggrid.tsx
+++ packages/builder-object/src/valueTypes/aggrid.tsx
@@ -9,14 +9,15 @@ import arrayMove from 'array-move';
 
 import {AgGridColumn, AgGridReact} from 'ag-grid-react';
 
-import React, { useState, useRef } from 'react';
+import React, { useState, useEffect, forwardRef, useImperativeHandle, useRef } from 'react';
 
 import 'ag-grid-community/dist/styles/ag-grid.css';
-import 'ag-grid-community/dist/styles/ag-theme-balham.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 
 import './aggrid.less';
 
 const ProFieldRenderer = (props: any) => {
+  console.log(props)
   const { 
     value, 
     valueType = 'text',
@@ -26,27 +27,49 @@ const ProFieldRenderer = (props: any) => {
     <ProField 
       mode='read'
       valueType={valueType} 
-      fieldSchema={fieldSchema}
+      fieldProps={{
+        field_schema: fieldSchema
+      }}
       text={value}
+      emptyText=''
       />
   ) 
 }
 
-const ProFieldEditor = (props: any) => {
+const ProFieldEditor = forwardRef((props: any, ref) => {
   const { 
-    value, 
     valueType = 'text',
     fieldSchema
   } = props;
+  const [value, setValue] = useState(props.value);
+
+  /* Component Editor Lifecycle methods */
+  useImperativeHandle(ref, () => {
+    return {
+        getValue() {
+            return value;
+        },
+        isPopup() {
+          return false;
+        }
+    };
+  });
+  
   return (
     <ProField 
       mode='edit'
       valueType={valueType} 
-      fieldSchema={fieldSchema}
-      text={value}
+      value={value}
+      onChange={(newValue)=>{
+        setValue(newValue)
+      }}
+      fieldProps={{
+        field_schema: fieldSchema
+      }}
       />
   ) 
-}
+});
+
 // 表格类型字段，
 // value格式：{ gridField: [{subField1: 666, subField2: 'yyy'}] }
 // 编辑时调用 editable protable，要求行可拖拉调整顺序
@@ -63,12 +86,9 @@ export const ObjectFieldGrid = (props) => {
   })
   const {sub_fields=[]} = fieldSchema;
   const [value, setValue] = useState<any>(initialValue && _.isArray(initialValue)? initialValue : [])
-  const [editableKeys, setEditableRowKeys] = useState<React.Key[]>(() =>
-    value.map((item) => item._id),
-  );
 
   const columns:any[] = [{
-    rowDrag: true,
+    rowDrag: mode == 'edit',
     width: 30,
   }];
   _.forEach(sub_fields, (field, fieldName)=>{
@@ -96,70 +116,33 @@ export const ObjectFieldGrid = (props) => {
       editable: !field.readonly,
     })
   });
-  // if (mode == 'edit'){
-  //   columns.push({
-  //     title: '',
-  //     valueType: 'option',
-  //     width: 50,
-  //     render: (text, record, _, action) => [
-  //       <a
-  //         key="delete"
-  //         onClick={() => {
-  //           setValue(value.filter((item) => item._id !== record._id));
-  //         }}
-  //       >
-  //         删除
-  //       </a>,
-  //     ],
-  //   })
-  // }
-  
 
-  const TableSortableItem = SortableElement(props => <tr {...props} />);
-  const DragHandle = SortableHandle(() => <MenuOutlined style={{ cursor: 'grab', color: '#999' }} />);
-
-  const onDragEnd = ({ oldIndex, newIndex }) => {
-    if (oldIndex !== newIndex) {
-      const newData = arrayMove([].concat(value), oldIndex, newIndex).filter(el => !!el);
-      onChange?.(newData);
-      setValue(newData);
-    }
-    // TODO: 拖动结果需要单独出发 form 保存事件。
+  const onCellClicked = ($event) => {
+    if (mode == 'read')
+      return;
+    $event.api.startEditingCell({
+      rowIndex: $event.rowIndex,
+      colKey: $event.column.colId
+    });
   };
 
-  const TableSortableContainer = SortableContainer(props => <tbody {...props} />);
-  const DraggableContainer = props => (
-    <TableSortableContainer
-      useDragHandle
-      disableAutoscroll
-      helperClass="row-dragging"
-      onSortEnd={onDragEnd}
-      {...props}
-    />
-  );
-
-  const DraggableBodyRow = ({ className, style, ...restProps }) => {
-    // function findIndex base on Table rowKey props and should always be a right array index
-    const index = value.findIndex(x => x._id === restProps['data-row-key']);
-    return <TableSortableItem index={index} {...restProps} />;
-  };
-
-  const editable: any = {
-    type: 'multiple',
-    editableKeys,
-    onChange: setEditableRowKeys,
-    actionRender: (row, _, dom) => {
-      return [dom.delete];
-    },
+  const onRowDragEnd = (e) => {
+    var rowData = [];
+    e.api.forEachNode(function (node) {
+      rowData.push(node.data);
+    });
+    onChange(rowData)
   };
 
   return (
-    <div className="ag-theme-balham">
+    <div className="ag-theme-alpine">
       <AgGridReact
         rowDragManaged={true}
         animateRows={true}
         rowData={value}
         columnDefs={columns}
+        onRowDragEnd={onRowDragEnd.bind(this)}
+        onCellClicked={onCellClicked}
         frameworkComponents = {{
           proFieldRenderer: ProFieldRenderer,
           proFieldEditor: ProFieldEditor,
