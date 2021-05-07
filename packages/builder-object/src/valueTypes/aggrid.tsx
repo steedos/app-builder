44390aee68462917f3efe754e35c2193b211b1ed
Jack Zhuang <50353452+hotlong@users.noreply.github.com>
2021-05-07 18:05:21 +0800

    row delete

--- packages/builder-object/src/valueTypes/aggrid.tsx
+++ packages/builder-object/src/valueTypes/aggrid.tsx
@@ -74,25 +74,6 @@ const ProFieldEditor = forwardRef((props: any, ref) => {
   ) 
 });
 
-const RowActions = (props: any) => {
-  return (
-    <Dropdown
-      assistiveText={{ icon: 'More Options' }}
-      iconCategory="utility"
-      iconName="down"
-      iconVariant="border-filled"
-      iconSize='x-small'
-      menuPosition="overflowBoundaryElement"
-      onSelect={(value) => {
-        console.log('selected: ', value);
-      }}
-      options={[
-        { label: 'Delete', value: 'delete' }
-      ]}
-    />
-  )
-}
-
 // 表格类型字段，
 // value格式：{ gridField: [{subField1: 666, subField2: 'yyy'}] }
 // 编辑时调用 editable protable，要求行可拖拉调整顺序
@@ -102,52 +83,39 @@ export const ObjectFieldGrid = (props) => {
   
   const {mode='read', text =[], fieldProps={}} = props;
   const { field_schema: fieldSchema = {}, depend_field_values: dependFieldValues={}, value:initialValue, onChange } = fieldProps;
-  
+
   _.forEach(initialValue, (row)=>{
     if (!row._id)
       row._id=uuidv4()
   })
-  const {sub_fields=[]} = fieldSchema;
   const [value, setValue] = useState<any>(initialValue && _.isArray(initialValue)? initialValue : [])
 
-  const columns:any[] = [{
-    rowDrag: mode == 'edit',
-    hide: !(mode == 'edit'),
-    width: 30,
-  }];
-  _.forEach(sub_fields, (field, fieldName)=>{
-    columns.push({
-      field: fieldName,
-      headerName: field.label?field.label:fieldName,
-      width: field.is_wide? 300: 150,
-      resizable: true,
-      filter: true,
-      cellRenderer: 'proFieldRenderer',
-      cellRendererParams: {
-        fieldSchema: field,
-        valueType: field.type,
-        mode
-      },
-      cellEditor: 'proFieldEditor',
-      cellEditorParams: {
-        fieldSchema: field,
-        valueType: field.type,
-        mode,
-      },
-      // key: fieldName,
-      // dataIndex: fieldName,
-      // title: field.label?field.label:fieldName,
-      // valueType: field.type,
-      editable: !field.readonly,
-    })
-  });
-  // 操作按钮
-  columns.push({
-    hide: !(mode == 'edit'),
-    width: 50,
-    cellEditor: 'rowActions',
-    cellRenderer: 'rowActions',
-  });
+  const RowActions = (props: any) => {
+    console.log(props)
+    return (
+      <Dropdown
+        assistiveText={{ icon: 'Options' }}
+        iconCategory="utility"
+        iconName="down"
+        iconVariant="border-filled"
+        iconSize='x-small'
+        menuPosition="overflowBoundaryElement"
+        onSelect={(option) => {
+          const selectedId = props.data?._id
+          if (option.value === 'delete') {
+            const newValue = value.filter(function (dataItem) {
+              return dataItem._id != selectedId
+            });
+            props.api.setRowData(newValue);
+            onChange(newValue)
+          }
+        }}
+        options={[
+          { label: 'Delete', value: 'delete' }
+        ]}
+      />
+    )
+  }
 
   const onCellClicked = ($event) => {
     if (mode == 'read')
@@ -166,13 +134,57 @@ export const ObjectFieldGrid = (props) => {
     onChange(rowData)
   };
 
+  const getColumns = ()=>{
+
+    const {sub_fields=[]} = fieldSchema;
+
+    const columns:any[] = [{
+      rowDrag: mode == 'edit',
+      hide: !(mode == 'edit'),
+      width: 30,
+    }];
+    _.forEach(sub_fields, (field, fieldName)=>{
+      columns.push({
+        field: fieldName,
+        headerName: field.label?field.label:fieldName,
+        width: field.is_wide? 300: 150,
+        resizable: true,
+        filter: true,
+        cellRenderer: 'proFieldRenderer',
+        cellRendererParams: {
+          fieldSchema: field,
+          valueType: field.type,
+          mode
+        },
+        cellEditor: 'proFieldEditor',
+        cellEditorParams: {
+          fieldSchema: field,
+          valueType: field.type,
+          mode,
+        },
+        // key: fieldName,
+        // dataIndex: fieldName,
+        // title: field.label?field.label:fieldName,
+        // valueType: field.type,
+        editable: !field.readonly,
+      })
+    });
+    // 操作按钮
+    columns.push({
+      hide: !(mode == 'edit'),
+      width: 50,
+      cellRenderer: 'rowActions',
+      cellEditor: 'rowActions',
+    });
+    return columns
+  }
   return (
     <div className="ag-theme-balham steedos-grid">
       <AgGridReact
         rowDragManaged={true}
         animateRows={true}
         rowData={value}
-        columnDefs={columns}
+        columnDefs={getColumns()}
         stopEditingWhenGridLosesFocus={true}
         onRowDragEnd={onRowDragEnd.bind(this)}
         onCellClicked={onCellClicked}
