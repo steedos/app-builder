32caf07e01fe91f1e2f69a7a32511a2c3edc7d2b
baozhoutao <baozhoutao@hotoa.com>
2021-05-15 17:45:36 +0800

    builder-object: export API \ SteedosRouter

--- packages/builder-object/src/index.tsx
+++ packages/builder-object/src/index.tsx
@@ -7,3 +7,6 @@ export * from './providers/SteedosProvider';
 export * from './providers/SteedosContext';
 export * from './utils/';
 export * from './valueTypes/';
+export { BrowserRouter as SteedosRouter } from "react-router-dom";
+export { API } from '@steedos/builder-store';
+// export { FooterToolbar as SteedosFormFooterToolbar} from '@ant-design/pro-layout';
