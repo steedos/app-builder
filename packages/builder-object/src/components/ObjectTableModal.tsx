c7d53f47b8e3d1ed7a1b6040a2f96fff815f8876
殷亮辉 <yinlianghui@hotoa.com>
2021-05-21 17:54:49 +0800

    ObjectTableModal调用ObjectModal去除重复代码

--- packages/builder-object/src/components/ObjectTableModal.tsx
+++ packages/builder-object/src/components/ObjectTableModal.tsx
@@ -1,110 +1,18 @@
-import React, { useState, useEffect, useContext, useMemo } from "react"
-import { Modal, ConfigProvider } from "antd"
-import { ObjectTable, ObjectTableProps } from ".."
-import type { ModalProps } from 'antd';
-import { createPortal } from 'react-dom';
+import React from "react"
+import { ObjectTable, ObjectModal, ObjectModalProps } from ".."
 import { omit } from "lodash"
 
-export type ModalCommonProps = {
-  trigger?: JSX.Element,
-  onFinish?: (value: any) => Promise<boolean | void>;
-  visible?: ModalProps['visible'];
-  onVisibleChange?: (visible: boolean) => void;
-  modalProps?: Omit<ModalProps, 'visible'>;
-  title?: ModalProps['title'];
-  width?: ModalProps['width'];
-}
-
 export type ObjectTableModalProps = {
-  isDrawer?: boolean
-  onChange?: (value: any) => void;
-} & ModalCommonProps & ObjectTableProps<any>
+} & Omit<ObjectModalProps, 'contentComponent'>
 
 export const ObjectTableModal = ({
-  isDrawer = false,
-  trigger,
-  onFinish,
-  onVisibleChange,
-  modalProps,
-  title,
-  width,
-  onChange,
   ...rest
 }: ObjectTableModalProps) => {
-  const [selectedRowKeys, setSelectedRowKeys] = useState([])
-  const [selectedRows, setSelectedRows] = useState([])
-  const [visible, setVisible] = useState<boolean>(!!rest.visible);
-  const context = useContext(ConfigProvider.ConfigContext);
-
-  useEffect(() => {
-    if (visible && rest.visible) {
-      onVisibleChange?.(true);
-    }
-  }, [visible]);
-
-  const handleOnChange = (keys: any, rows: any) => {
-    setSelectedRowKeys(keys);
-    setSelectedRows(rows);
-    onChange && onChange(selectedRowKeys, selectedRows);
-  }
-
-  const renderDom = useMemo(() => {
-    if (modalProps?.getContainer) {
-      if (typeof modalProps?.getContainer === 'function') {
-        return modalProps?.getContainer?.();
-      }
-      if (typeof modalProps?.getContainer === 'string') {
-        return document.getElementById(modalProps?.getContainer);
-      }
-      return modalProps?.getContainer;
-    }
-    return context?.getPopupContainer?.(document.body);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [context, modalProps, visible]);
 
   return (
-    <>
-      {createPortal(
-        <div onClick={(e) => e.stopPropagation()}>
-          <Modal
-            title={title}
-            width={width || 800}
-            {...modalProps}
-            getContainer={false}
-            visible={visible}
-            onCancel={(e) => {
-              setVisible(false);
-              modalProps?.onCancel?.(e);
-            }}
-            onOk={async (e) => {
-              if (!onFinish) {
-                setVisible(false);
-                modalProps?.onOk?.(e);
-                return;
-              }
-              const success = await onFinish(selectedRowKeys, selectedRows);
-              if (success !== false) {
-                setVisible(false);
-                modalProps?.onOk?.(e);
-              }
-            }}
-          >
-            <ObjectTable
-              {...omit(rest, ['visible', 'title', 'onChange'])}
-              onChange={handleOnChange}
-            />
-          </Modal>
-        </div>,
-        renderDom || document.body,
-      )}
-      {trigger &&
-        React.cloneElement(trigger, {
-          ...trigger.props,
-          onClick: (e: any) => {
-            setVisible(!visible);
-            trigger.props?.onClick?.(e);
-          },
-        })}
-    </>
+    <ObjectModal
+      contentComponent={ObjectTable}
+      {...omit(rest, ['contentComponent'])}
+    />
   )
 }
