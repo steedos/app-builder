7177874a9a8d06e0174314e4a6fb92a458476009
殷亮辉 <yinlianghui@hotoa.com>
2021-05-21 13:25:26 +0800

    Update ObjectTableModal.tsx

--- packages/builder-object/src/components/ObjectTableModal.tsx
+++ packages/builder-object/src/components/ObjectTableModal.tsx
@@ -1,11 +1,3 @@
-/*
-该组件是一个模式弹窗组件包裹了一个ObjectExpandTable组件.
-除了show,onClose,onChange这三个参数外,其它的参数直接透传给了ObjectExpandTable
- * @Author: Kent.Wood 
- * @Date: 2021-03-25 22:38:48 
- * @Last Modified by: Kent.Wood
- * @Last Modified time: 2021-03-25 22:39:59
- */
 import React, { useState, useEffect, useContext, useMemo } from "react"
 import { Modal, ConfigProvider } from "antd"
 import { ObjectTable, ObjectTableProps } from ".."
