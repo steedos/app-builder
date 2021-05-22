a0693aaebd9a6d2ae38f0fc4318e493e0e0e28d1
Jack Zhuang <50353452+hotlong@users.noreply.github.com>
2021-05-22 18:41:46 +0800

    Update aggrid.stories.tsx

--- packages/builder-ag-grid/stories/aggrid.stories.tsx
+++ packages/builder-ag-grid/stories/aggrid.stories.tsx
@@ -1,13 +1,5 @@
-import { ObjectGrid} from "@steedos/builder-aggrid";
-import { FieldSection } from "@steedos/builder-form";
+import { ObjectGrid} from "@steedos/builder-ag-grid";
 import * as React from "react"
-import { API } from '@steedos/builder-store';
-import { Link } from "react-router-dom";
-import { Modal, TreeSelect, Select, Input, Button } from "antd"
-import ProCard from "@ant-design/pro-card"
-import queryString from "querystring"
-import { useEffect, useState } from "react";
-
 export default {
   title: "Object Table AG Grid",
 }
