1ab473cb3dcb196f408cdb934774d5189d4d05bd
殷亮辉 <yinlianghui@hotoa.com>
2021-05-24 13:34:54 +0800

    选组支持权限判断

--- packages/builder-object/src/components/Organizations.tsx
+++ packages/builder-object/src/components/Organizations.tsx
@@ -1,26 +1,52 @@
 import React from "react"
-import { ObjectTree, ObjectTreeProps } from ".."
 import { observer } from "mobx-react-lite"
 import { omit } from "lodash"
+import { User } from '@steedos/builder-store';
+import { Alert, Spin } from 'antd';
+import { ObjectTree, ObjectTreeProps } from ".."
 
 export type OrganizationsProps = {
 } & ObjectTreeProps
 
 export const Organizations = observer(({
   columnFields,
+  filters: defaultFilters,
   ...rest
 }: OrganizationsProps) => {
+
+  const userSession = User.getSession();
+  let filters: any = ["hidden", "!=", true];
+  let errorMessage:any;
+  if (User.isLoading){
+    console.log("Loading session...")
+    return (<div><Spin/></div>);
+  }
+  else{
+    if(!userSession.is_space_admin){
+      const orgIds = User.getCompanyOrganizationIds();
+      if(orgIds && orgIds.length){
+        // 不是管理员时，要限定范围为当前用户所属分部关联组织内
+        filters = [filters, [["_id", "=", orgIds], "or", ["parents", "=", orgIds]]];
+      }
+      else{
+        console.error('您的账户未分配到任何分部，无法查看通讯录信息，请联系系统管理员！')
+        errorMessage = <Alert message="您的账户没有权限访问此内容，请联络系统管理员。" type="warning" />
+      }
+    }
+  }
   let props = {
     objectApiName: "organizations",
     nameField: "name",
     parentField: "parent",
-    // releatedColumnField: "organizations_parents",
-    // filters: expandDefine.expandFilters
+    filters
   };
   return (
-    <ObjectTree
-      {...props}
-      {...omit(rest, ['objectApiName'])}
-    />
+    ( 
+      errorMessage ? errorMessage :
+      <ObjectTree
+        {...props}
+        {...omit(rest, ['objectApiName'])}
+      />
+    )
   )
 });
