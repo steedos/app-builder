3e30ce3ab70f0e263becb5d4d97f4d1c693c6067
廖大雪 <2291335922@qq.com>
2021-05-25 10:56:47 +0800

    tree-select 右边选项加权限和筛选条件

--- packages/builder-object/src/components/SpaceUsers.tsx
+++ packages/builder-object/src/components/SpaceUsers.tsx
@@ -1,4 +1,6 @@
 import React from "react"
+import { Alert, Spin } from 'antd';
+import { User } from '@steedos/builder-store';
 import { ObjectExpandTable, ObjectExpandTableProps } from ".."
 import { observer } from "mobx-react-lite"
 import { omit } from "lodash"
@@ -8,8 +10,35 @@ export type SpaceUsersProps = {
 
 export const SpaceUsers = observer(({
   columnFields,
+  filters,
   ...rest
 }: SpaceUsersProps) => {
+  const userSession = User.getSession();
+  let defaultSaceUsersFilters: any = ["user_accepted", "=", true];
+  let errorMessage:any;
+  if (User.isLoading){
+    // 这里不可以直接return (<div>Loading</div>) 上面有调用useRef
+    console.log("Loading session...")
+  }
+  else{
+    if(!userSession.is_space_admin){
+      const orgIds = User.getCompanyOrganizationIds();
+      if(orgIds && orgIds.length){
+        // orgExpandFilters = [orgExpandFilters, [["_id", "=", orgIds], "or", ["parents", "=", orgIds]]];
+        // 不是管理员时，要限定右侧用户范围为当前用户所属分部关联组织内
+        defaultSaceUsersFilters = [defaultSaceUsersFilters, ["organizations_parents", "=", orgIds]];
+        if(filters && filters.length){
+          filters = [filters, defaultSaceUsersFilters]
+        }else{
+          filters = defaultSaceUsersFilters;
+        }
+      }
+      else{
+        console.error('您的账户未分配到任何分部，无法查看通讯录信息，请联系系统管理员！')
+        errorMessage = <Alert message="您的账户没有权限访问此内容，请联络系统管理员。" type="warning" />
+      }
+    }
+  }
   let props = {
     columnFields
   };
@@ -24,10 +53,13 @@ export const SpaceUsers = observer(({
     }]
   }
   return (
-    <ObjectExpandTable
-      objectApiName="space_users"
-      {...props}
-      {...omit(rest, ['objectApiName'])}
-    />
+    User.isLoading ? (<Spin />) : ( errorMessage ? errorMessage :
+      <ObjectExpandTable
+        filters={filters}
+        objectApiName="space_users"
+        {...props}
+        {...omit(rest, ['objectApiName'])}
+        />
+      )
   )
 });
