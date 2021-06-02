a1d149f9c3ce83673d60b23beed8f264c720ebac
殷亮辉 <yinlianghui@hotoa.com>
2021-06-02 11:45:12 +0800

    community中公开相关组件

--- packages/builder-community/src/index.tsx
+++ packages/builder-community/src/index.tsx
@@ -1,4 +1,5 @@
 import { ComponentRegistry } from '@steedos/builder-store';
+
 export { ObjectTable, ObjectForm, API, SteedosProvider, SteedosRouter, Forms, ObjectListView} from '@steedos/builder-object';
 export { ObjectGrid } from '@steedos/builder-ag-grid';
 export { ComponentRegistry }
