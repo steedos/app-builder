50ab00a797c333187adc1e743e8202976f984218
殷亮辉 <yinlianghui@hotoa.com>
2021-06-02 11:50:35 +0800

    community中公开相关组件

--- packages/builder-community/src/index.tsx
+++ packages/builder-community/src/index.tsx
@@ -1,6 +1,8 @@
 import { ComponentRegistry } from '@steedos/builder-store';
+import { ObjectTable, ObjectForm, ObjectListView} from '@steedos/builder-object';
+
+Object.assign(ComponentRegistry.components,{ ObjectForm, ObjectTable, ObjectListView });
 
 export { ObjectTable, ObjectForm, API, SteedosProvider, SteedosRouter, Forms, ObjectListView} from '@steedos/builder-object';
 export { ObjectGrid } from '@steedos/builder-ag-grid';
 export { ComponentRegistry }
-(window as any).ComponentRegistry = ComponentRegistry;
