8f052c96a9e2a02cad430274e22bb717dc7a09dd
廖大雪 <2291335922@qq.com>
2021-06-04 18:49:57 +0800

    添加相关组件

--- packages/builder-community/src/index.tsx
+++ packages/builder-community/src/index.tsx
@@ -1,7 +1,18 @@
 import { ComponentRegistry } from '@steedos/builder-store';
-import { ObjectTable, ObjectForm, ObjectListView} from '@steedos/builder-object';
+import { ObjectTable, ObjectProTable,
+    ObjectForm, ObjectListView,
+    SpaceUsers, SpaceUsersModal,
+    Organizations, OrganizationsModal,
+    ObjectTree, ObjectExpandTable, ObjectModal
+} from '@steedos/builder-object';
 
-Object.assign(ComponentRegistry.components,{ ObjectForm, ObjectTable, ObjectListView });
+Object.assign(ComponentRegistry.components,{ 
+    ObjectTable, ObjectProTable,
+    ObjectForm, ObjectListView,
+    SpaceUsers, SpaceUsersModal,
+    Organizations, OrganizationsModal,
+    ObjectTree, ObjectExpandTable, ObjectModal
+});
 
 export { ObjectTable, ObjectForm, API, SteedosProvider, SteedosRouter, Forms, ObjectListView} from '@steedos/builder-object';
 export { ObjectGrid } from '@steedos/builder-ag-grid';
