00b96d24b8a9f99a0afd6a99e30c39b074fba2d8
殷亮辉 <yinlianghui@hotoa.com>
2021-06-07 17:05:05 +0800

    #57 自动编号字段支持is_name显示为链接

--- packages/builder-object/src/valueTypes/autonumber.tsx
+++ packages/builder-object/src/valueTypes/autonumber.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
+import ProField from "@ant-design/pro-field";
 
 // 自动编号类型字段
 // value 值为服务端自动生成的字符串
 // 编辑时只读。显示时直接显示 value
 export const autonumber = {
   render: (text: any, props: any) => {
-    return (<span>{text}</span>)
+    return (
+      <ProField mode='read' valueType='text' {...props} />
+    )
   },
   renderFormItem: (_: any, props: any) => {
-    return (<span></span>)
+    return (
+      <ProField mode='read' valueType='text' {...props} />
+    )
   }
 }
