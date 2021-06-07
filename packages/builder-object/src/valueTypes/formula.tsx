d6129f7fd49be87304e27df6cfec4e23fdff1b52
廖大雪 <2291335922@qq.com>
2021-06-07 18:10:32 +0800

    #57 formula、summary字段支持is_name显示为链接

--- packages/builder-object/src/valueTypes/formula.tsx
+++ packages/builder-object/src/valueTypes/formula.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
+import ProField from "@ant-design/pro-field";
 
 // 公式类型类型字段
 // value 值为服务端自动生成的字符串
 // 编辑时只读。显示时直接显示 value
 export const formula = {
   render: (text: any, props: any) => {
-    return (<span>{text}</span>)
+    return (
+      <ProField mode='read' valueType='text' {...props} />
+    )
   },
   renderFormItem: (text: any, props: any) => {
-    return (<span>{text}</span>)
+    return (
+      <ProField mode='read' valueType='text' {...props} />
+    )
   }
 }
--- packages/builder-object/src/valueTypes/summary.tsx
+++ packages/builder-object/src/valueTypes/summary.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import ProField from "@ant-design/pro-field";
 
 export const summary = {
   render: (text: any, props: any) => {
-    return (<span>{text}</span>)
+    return (
+      <ProField mode='read' valueType='text' {...props} />
+    )
   },
   renderFormItem: (text: any, props: any) => {
-    return (<span>{text}</span>)
+    return (
+      <ProField mode='read' valueType='text' {...props} />
+    )
   }
 }
