49fe1be5db78cdc41e578df4be65fd2c65389bca
殷亮辉 <yinlianghui@hotoa.com>
2021-06-08 10:11:53 +0800

    Fix valueType number_range 转换后过滤条件值未存入store

--- packages/builder-sdk/src/filters.ts
+++ packages/builder-sdk/src/filters.ts
@@ -17,8 +17,14 @@ export const convertFormToFilters = (objectSchema: any, formValues: any)=>{
             else if(["boolean", "lookup", "master_detail", "select", "toggle"].indexOf(field.type) > -1){
                 return [k, "=", v];
             }
+            else if(["number", "currency"].indexOf(field.type) > -1){
+                return [k, "=", v];
+            }
+            else if(["number_range", "currency_range"].indexOf(field.type) > -1){
+                return [k, "between", v];
+            }
             else if(["date", "datetime"].indexOf(field.type) > -1){
-                return [k, "between", [v, v]];
+                return [k, "=", v];
             }
             else if(["date_range", "datetime_range"].indexOf(field.type) > -1){
                 return [k, "between", v];
