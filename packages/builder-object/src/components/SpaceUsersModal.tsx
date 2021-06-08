054053c8e82f175f095540765bf38bbeb613ba97
廖大雪 <2291335922@qq.com>
2021-06-08 10:44:50 +0800

    ag-grid中手机号字段与姓名、邮箱字段保持一致

--- packages/builder-object/src/components/SpaceUsersModal.tsx
+++ packages/builder-object/src/components/SpaceUsersModal.tsx
@@ -66,7 +66,7 @@ export const SpaceUsersModal = ({
       ...expandProps,
     },{
       fieldName: "mobile",
-      hideInSearch: true,
+      hideInSearch: isMobile ? true : false,
     }]
   }
 
