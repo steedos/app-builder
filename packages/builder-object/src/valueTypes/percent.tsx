3862733f36d66e441df181673ff529cdd52c39f2
殷亮辉 <yinlianghui@hotoa.com>
2021-06-11 14:46:05 +0800

    #68 百分比字段跟之前规则一样支持小数位数

--- packages/builder-object/src/valueTypes/percent.tsx
+++ packages/builder-object/src/valueTypes/percent.tsx
@@ -1,28 +1,31 @@
 import React, { Fragment, useMemo } from 'react';
-
+import { FieldNumber } from "./number";
 import toNumber from 'lodash.tonumber';
-import ProField from "@ant-design/pro-field";
 
 // 百分比类型字段
-// value 值为数字
+// value 值为数字，值显示为带百分号的字符串
+// 配置的小数位数指的是显示为百分比后的小数位数，比如20.50%表示小数位数为2，值为0.205，编辑时按4位小数位处理
 // 编辑和显示时需要处理小数位数
 export const percent = {
   render: (text: any, props: any) => {
-
-    const realValue = useMemo(
-      () =>
-        typeof text === 'string' && (text as string).includes('%')
-          ? 100*toNumber((text as string).replace('%', ''))
-          : 100*toNumber(text),
-      [text],
-    );
+    const { fieldProps } = props;
+    let value= fieldProps.value || text;//ProTable那边fieldProps.value没有值，只能用text
+    let newFieldProps = Object.assign({}, fieldProps, {
+      value: toNumber(value) * 100
+    });
     return (
-      <span>{realValue}%</span>
+      <span>{<FieldNumber mode='read' {...props} fieldProps={newFieldProps} />}%</span>
     )
   },
   renderFormItem: (_: any, props: any) => {
+    const { fieldProps } = props;
+    const { field_schema: fieldSchema = {} } = fieldProps;
+    let scale = (fieldSchema.scale || 0) + 2;//编辑时控件中小数位数始终比配置的位数多2位
+    let newFieldProps = Object.assign({}, fieldProps, {
+      field_schema: {...fieldSchema, scale}
+    });
     return (
-      <ProField mode='edit' valueType='digit' {...props} />
+      <FieldNumber mode='edit' {...props} fieldProps={newFieldProps} />
     )
   }
 }
