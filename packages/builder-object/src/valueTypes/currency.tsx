ac4efe6c6922ce3f28d6c59bd5511ff623eba5ac
殷亮辉 <yinlianghui@hotoa.com>
2021-06-11 16:49:40 +0800

    #68 公式字段按其dataType属性使用相关控件显示数据，支持小数位数显示

--- packages/builder-object/src/valueTypes/currency.tsx
+++ packages/builder-object/src/valueTypes/currency.tsx
@@ -1,18 +1,31 @@
 import React from 'react';
 import { FieldNumber } from "./number";
+import { isNil} from 'lodash';
 
 // 金额类型字段，跟数值字段比，只是默认小数位数变成2位了
 // value 值为数字
 // 编辑和显示时需要处理小数位数
 export const currency = {
   render: (text: any, props: any) => {
+    const { fieldProps } = props;
+    const { field_schema: fieldSchema = {} } = fieldProps;
+    let scale = isNil(fieldSchema.scale) ? 2 : fieldSchema.scale;
+    let newFieldProps = Object.assign({}, fieldProps, {
+      field_schema: {...fieldSchema, scale}
+    });
     return (
-      <FieldNumber mode='read' {...props} />
+      <FieldNumber mode='read' {...props} fieldProps={newFieldProps} />
     )
   },
   renderFormItem: (_: any, props: any) => {
+    const { fieldProps } = props;
+    const { field_schema: fieldSchema = {} } = fieldProps;
+    let scale = isNil(fieldSchema.scale) ? 2 : fieldSchema.scale;
+    let newFieldProps = Object.assign({}, fieldProps, {
+      field_schema: {...fieldSchema, scale}
+    });
     return (
-      <FieldNumber mode='edit' {...props} />
+      <FieldNumber mode='edit' {...props} fieldProps={newFieldProps} />
     )
   }
 }
--- packages/builder-object/src/valueTypes/formula.tsx
+++ packages/builder-object/src/valueTypes/formula.tsx
@@ -1,18 +1,38 @@
 import React from 'react';
 import ProField from "@ant-design/pro-field";
 
+const getFormulaFieldType = (dataType: string)=>{
+  return dataType;
+}
+
+export const FieldFormula = (props:any) => {
+  const { mode, fieldProps } = props;
+  const { field_schema: fieldSchema = {} } = fieldProps;
+  const valueType = getFormulaFieldType(fieldSchema.data_type);
+  if(mode === "read"){
+    return (
+      <ProField {...props} mode='read' valueType={valueType} />
+    )
+  }
+  else{
+    return (
+      <ProField {...props} mode='read' valueType={valueType} />
+    )
+  }
+};
+
 // 公式类型类型字段
 // value 值为服务端自动生成的字符串
 // 编辑时只读。显示时直接显示 value
 export const formula = {
   render: (text: any, props: any) => {
     return (
-      <ProField mode='read' valueType='text' {...props} />
+      <FieldFormula {...props} />
     )
   },
   renderFormItem: (text: any, props: any) => {
     return (
-      <ProField valueType='text' {...props} mode='read' />
+      <FieldFormula {...props} />
     )
   }
 }
