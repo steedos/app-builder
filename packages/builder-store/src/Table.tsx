112130ba2173f28825d7dc1e8f957aba59c2d018
殷亮辉 <yinlianghui@hotoa.com>
2021-06-16 15:26:17 +0800

    #83 优化store中的Table以解决相关bug

--- packages/builder-store/src/Table.tsx
+++ packages/builder-store/src/Table.tsx
@@ -1,25 +1,66 @@
 import { types } from "mobx-state-tree";
-
-export const TableRowModel = types.model({
-  id: types.identifier,
-  name: types.string,
-  required: types.boolean,
-  readonly: types.boolean,
-})
+import { pullAllBy, differenceBy, remove } from "lodash";
 
 export const TableModel = types.model({
   id: types.identifier,
   // selectedRows: types.union(types.array(TableRowModel), types.undefined),
   // selectedRows: types.union(types.array(types.frozen()), types.undefined)
+  rowKey: types.union(types.string, types.undefined),
   selectedRows: types.frozen()
-}).actions(self => ({
-  setSelectedRows(rows: any) {
+}).actions(self => {
+  const getRowKey = () => {
+    return self.rowKey || "_id";
+  };
+  const setSelectedRows = (rows: any) => {
     self.selectedRows = rows
-  },
-  getSelectedRows() {
+  };
+  const getSelectedRows = () => {
     return self.selectedRows || [];
+  };
+  const addSelectedRows = (rows: any) => {
+    const rowKey = getRowKey();
+    const rowsForAdd = differenceBy(rows, self.selectedRows, rowKey);
+    self.selectedRows = self.selectedRows.concat(rowsForAdd);
+  };
+  const addSelectedRow = (row: any) => {
+    addSelectedRows([row]);
+  };
+  const addSelectedRowsByKeys = (keys: any, columns: any = ["name"]) => {
+    
+  };
+  const addSelectedRowByKey = (key: any, columns: any) => {
+    addSelectedRowsByKeys([key], columns);
+  };
+  const removeSelectedRows = (rows: any) => {
+    const rowKey = getRowKey();
+    pullAllBy(self.selectedRows, rows, rowKey)
+  };
+  const removeSelectedRow = (row: any) => {
+    removeSelectedRows([row]);
+  };
+  const removeSelectedRowsByKeys = (keys: any) => {
+    const rowKey = getRowKey();
+    self.selectedRows = remove(self.selectedRows, function(n) {
+      return keys.indexOf(n[rowKey]) > -1;
+    });
+  };
+  const removeSelectedRowByKey = (key: any) => {
+    removeSelectedRowsByKeys([key]);
+  };
+  return {
+    getRowKey,
+    setSelectedRows,
+    getSelectedRows,
+    addSelectedRows,
+    addSelectedRow,
+    addSelectedRowsByKeys,
+    addSelectedRowByKey,
+    removeSelectedRows,
+    removeSelectedRow,
+    removeSelectedRowsByKeys,
+    removeSelectedRowByKey
   }
-}))
+})
 
 export const Tables = types.model({
   items: types.optional(types.map(TableModel), {})
