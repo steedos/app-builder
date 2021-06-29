c1451c43e74d7f6bf6d8b0a9d9c6c220da31429e
廖大雪 <2291335922@qq.com>
2021-06-29 17:07:48 +0800

    优化select组件代码

--- packages/builder-object/src/valueTypes/select.tsx
+++ packages/builder-object/src/valueTypes/select.tsx
@@ -1,88 +1,74 @@
 import React, { useState } from 'react'
-import { isFunction, filter,} from 'lodash';
+import { isFunction, filter } from 'lodash';
 import FieldSelect from '@ant-design/pro-field/es/components/Select';
 import { safeRunFunction } from '@steedos/builder-sdk';
+import { observer } from "mobx-react-lite";
 
-export const select = {
-  render: (text: any, props: any)=> {
-    const { valueType, mode, fieldProps, ...rest } = props;
-    const { field_schema: fieldSchema = {},depend_field_values: dependFieldValues={} } = fieldProps;
-    let tags:any;
-    let options = fieldSchema.optionsFunction ? fieldSchema.optionsFunction : fieldSchema.options ;
-    const value = fieldProps.value || props.text;//ProTable那边fieldProps.value没有值，只能用text
-    // 按原来lookup控件的设计，this.template.data._value为原来数据库中返回的选项值，this.template.data.value为当前用户选中的选项
-    const optionsFunctionThis = {
-      template:{
-        data: {
-          value: value,
-          _value: value
-        }
-      } 
-    };
-    const objectApiName = props.object_api_name;
-    let optionsFunctionValues = Object.assign({}, dependFieldValues, {
-        // space: Settings.tenantId,
-        _object_name: objectApiName
-    });
-    options = isFunction(options) ? safeRunFunction(options,[optionsFunctionValues],[], optionsFunctionThis) : options;
-    tags = filter(options,(optionItem: any)=>{
-        return fieldSchema.multiple ? value.indexOf(optionItem.value) > -1 : optionItem.value === value;
+export const SelectField = observer((props: any) => {
+  const { valueType, mode, fieldProps = {}, ...rest } = props;
+  const [params, setParams] = useState({ open: false, openTag: null });
+  const { field_schema: fieldSchema = {}, depend_field_values: dependFieldValues = {} } = fieldProps;
+  const { multiple , optionsFunction } = fieldSchema;
+  let options = optionsFunction ? optionsFunction : fieldSchema.options;
+  const value = fieldProps.value || props.text;//ProTable那边fieldProps.value没有值，只能用text
+  // 按原来lookup控件的设计，this.template.data._value为原来数据库中返回的选项值，this.template.data.value为当前用户选中的选项
+  const optionsFunctionThis = {
+    template: {
+      data: {
+        value: value,
+        _value: value
+      }
+    }
+  };
+  const objectApiName = props.object_api_name;
+  let optionsFunctionValues = Object.assign({}, dependFieldValues, {
+    // space: Settings.tenantId,
+    _object_name: objectApiName
+  });
+  
+  if (mode === 'read') {
+    let tags = [];
+    options = isFunction(options) ? safeRunFunction(options, [optionsFunctionValues], [], optionsFunctionThis) : options;
+    tags = filter(options, (optionItem: any) => {
+      return multiple ? value.indexOf(optionItem.value) > -1 : optionItem.value === value;
     })
-    return (<React.Fragment>{tags.map((tagItem, index)=>{
-      let colorStyle:any = {
+    const tagsDom = tags.map((tagItem, index) => {
+      let colorStyle: any = {
         borderRadius: '10px',
         padding: '1px 6px',
         border: '1px',
       }
-      if(tagItem.color && tagItem.color.length){
-        colorStyle.background='#'+tagItem.color;
+      if (tagItem.color && tagItem.color.length) {
+        colorStyle.background = '#' + tagItem.color;
       }
       return (
         <React.Fragment key={tagItem.value}>
-            {index > 0 && ' '}
-            <span style={{...colorStyle}} >
-              {tagItem.label}
-            </span>
+          {index > 0 && ' '}
+          <span style={{ ...colorStyle }} >
+            {tagItem.label}
+          </span>
         </React.Fragment>
-    )})}</React.Fragment>)
-  },
-  renderFormItem: (text: any, props: any) => {
-    const [params, setParams] = useState({open: false,openTag: null});
-    const { fieldProps={} } = props;
-    const { field_schema: fieldSchema = {},depend_field_values: dependFieldValues={} } = fieldProps;
-    const { multiple ,optionsFunction} = fieldSchema;
-    let options = optionsFunction ? optionsFunction : fieldSchema.options;
-    const value= fieldProps.value || props.text;//ProTable那边fieldProps.value没有值，只能用text
-    // 按原来lookup控件的设计，this.template.data._value为原来数据库中返回的选项值，this.template.data.value为当前用户选中的选项
-    const optionsFunctionThis = {
-      template:{
-        data: {
-          value: value,
-          _value: value
-        }
-      } 
-    };
-    const objectApiName = props.object_api_name;
-    let optionsFunctionValues = Object.assign({}, dependFieldValues, {
-        // space: Settings.tenantId,
-        _object_name: objectApiName
+      )
     });
-    if (multiple){
+    return (<React.Fragment>{tagsDom}</React.Fragment>)
+  } 
+  else {
+    if (multiple) {
       fieldProps.mode = 'multiple';
     }
-    let proFieldProps:any = {};
-    let request:any;
-    let onDropdownVisibleChange:any;
+    let proFieldProps: any = {};
+    let request: any;
+    let onDropdownVisibleChange: any;
 
-    if(isFunction(options)){
+    if (isFunction(options)) {
       request = async (params: any, props: any) => {
         optionsFunctionValues._keyWords = params.keyWords;
-        const results = await safeRunFunction(options,[optionsFunctionValues],[], optionsFunctionThis);
+        const results = await safeRunFunction(options, [optionsFunctionValues], [], optionsFunctionThis);
         return results;
       };
       onDropdownVisibleChange = (open: boolean) => {
         if (open) {
-            setParams({ open, openTag: new Date() });
+          setParams({ open, openTag: new Date() });
         }
       }
       proFieldProps = {
@@ -90,15 +76,24 @@ export const select = {
         params,
         onDropdownVisibleChange,
       }
-    }else if(options){
+    } else if (options) {
       //options为空时不能直接覆盖fieldProps.options中的值，因为要允许直接给控件fieldProps.options赋值
       props.fieldProps.options = options;
     }
-    proFieldProps.showSearch=true;
-    proFieldProps.optionFilterProp='label';
+    proFieldProps.showSearch = true;
+    proFieldProps.optionFilterProp = 'label';
     // TODO: multiple：如果是true, 后期 需要 支持对已选中项进行拖动排序
     return (
       <FieldSelect mode='edit' {...props} {...proFieldProps} />
     )
+  }
+})
+
+export const select = {
+  render: (text: any, props: any) => {
+    return (<SelectField {...props} mode="read" />)
   },
+  renderFormItem: (text: any, props: any) => {
+    return (<SelectField {...props} mode="edit" />)
+  }
 }
