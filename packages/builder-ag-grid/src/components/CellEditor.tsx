c95a1ee15a507c47463fd8f5a5df34251fdcc8f2
baozhoutao <baozhoutao@hotoa.com>
2021-06-29 14:57:21 +0800

    优化退出编辑模式的时机。支持select\lookup多选
    https://github.com/steedos/steedos-project-pcmes/issues/106

--- packages/builder-ag-grid/src/components/CellEditor.tsx
+++ packages/builder-ag-grid/src/components/CellEditor.tsx
@@ -2,6 +2,49 @@ import React, { useContext, useRef, useEffect, useState, useImperativeHandle, fo
 import _ from "lodash"
 import ProField from "@ant-design/pro-field";
 
+function getParentsClassName(element, classNames=[]){
+  if(element){
+    classNames.push(element.className)
+    if(element.parentElement){
+      classNames = classNames.concat(getParentsClassName(element.parentElement))
+    }
+  }
+  return classNames
+}
+
+function useOnClickOutside(ref, handler) {
+  useEffect(
+    () => {
+      const listener = (event) => {
+        // Do nothing if clicking ref's element or descendent elements
+        console.log(`ref.current`, ref.current);
+        if (!ref.current || ref.current.contains(event.target)) {
+          return;
+        }
+        const parentsClassName = getParentsClassName(event.target)
+        console.log(`event.target`, parentsClassName)
+        if(parentsClassName.toString().indexOf('ant-modal-root') > -1 || parentsClassName.toString().indexOf('ant-select-dropdown') > -1){
+          return;
+        }
+        handler(event);
+      };
+      document.addEventListener("mousedown", listener);
+      document.addEventListener("touchstart", listener);
+      return () => {
+        document.removeEventListener("mousedown", listener);
+        document.removeEventListener("touchstart", listener);
+      };
+    },
+    // Add ref and handler to effect dependencies
+    // It's worth noting that because passed in handler is a new ...
+    // ... function on every render that will cause this effect ...
+    // ... callback/cleanup to run every render. It's not a big deal ...
+    // ... but to optimize you can wrap handler in useCallback before ...
+    // ... passing it into this hook.
+    [ref, handler]
+  );
+}
+
 export const AgGridCellEditor = forwardRef((props: any, ref) => {
   const { 
     valueType = 'text',
@@ -11,20 +54,33 @@ export const AgGridCellEditor = forwardRef((props: any, ref) => {
   } = props;
   const editedMap: any= context?.editedMap
   const [value, setValue] = useState(props.value);
+  const fieldRef = useRef(null)
+  const refEditor = useRef(null)
   /* Component Editor Lifecycle methods */
   useImperativeHandle(ref, () => {
     return {
         getValue() {
             return value;
         },
-        isPopup() {
+        isPopup() {    
           return true;
         }
-    };
+    }; 
   });
-  
+
+  useOnClickOutside(refEditor, (e) => {
+    setTimeout(() => props.api.stopEditing(false), 100);
+  });
+
+  // const IntervalID = setInterval(()=>{
+  //   console.log(`fieldRef.current`, document.activeElement, fieldRef.current.input)
+  //   if(fieldRef.current && document.activeElement != fieldRef.current.input){
+  //     props.api.stopEditing(false);
+  //     clearInterval(IntervalID);
+  //   }
+  // }, 300)
   return (
-    <section className="slds-popover slds-popover slds-popover_edit" role="dialog">
+    <section className="slds-popover slds-popover slds-popover_edit" role="dialog" ref={refEditor}>
       <div className="slds-popover__body">
         <ProField 
           mode='edit'
@@ -46,18 +102,21 @@ export const AgGridCellEditor = forwardRef((props: any, ref) => {
             // if (['lookup','select','master_detail'].indexOf(valueType)>=0){
             //   setTimeout(() => props.api.stopEditing(false), 100);
             // }
-            if(!element?.target || document.activeElement != element.target){
-              setTimeout(() => props.api.stopEditing(false), 100);
-            }
 
-            if(element?.target && document.activeElement === element.target){
-              
-              const IntervalID = setInterval(()=>{
-                if(document.activeElement != element.target){
-                  props.api.stopEditing(false);
-                  clearInterval(IntervalID);
-                }
-              }, 300)
+            if(fieldSchema.multiple != true){
+              if(!element?.target || document.activeElement != element.target){
+                setTimeout(() => props.api.stopEditing(false), 100);
+              }
+  
+              if(element?.target && document.activeElement === element.target){
+                
+                const IntervalID = setInterval(()=>{
+                  if(document.activeElement != element.target){
+                    props.api.stopEditing(false);
+                    clearInterval(IntervalID);
+                  }
+                }, 300)
+              }
             }
             
           }}
