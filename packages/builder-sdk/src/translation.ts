c187db6c47ca9235553a745b9436b569c7206a64
baozhoutao <baozhoutao@hotoa.com>
2021-06-30 16:37:49 +0800

    Update translation.ts

--- packages/builder-sdk/src/translation.ts
+++ packages/builder-sdk/src/translation.ts
@@ -1,4 +1,4 @@
-export function translate(key, options, locale){
+export function translate(key, options?, locale?){
     if(window.Creator && window.TAPi18n){
         return window.TAPi18n.__(key, options, locale)
     }else{
