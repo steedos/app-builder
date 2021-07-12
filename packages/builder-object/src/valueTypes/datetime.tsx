20a01ad4e36327ed745aaa540940415571b9f608
廖大雪 <2291335922@qq.com>
2021-07-12 10:02:30 +0800

    #162 datetime字段支持设置默认值

--- packages/builder-object/src/valueTypes/datetime.tsx
+++ packages/builder-object/src/valueTypes/datetime.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ProField from "@ant-design/pro-field";
 import moment from 'moment';
 import FieldDatePicker from "@ant-design/pro-field/es/components/DatePicker";
-// import "moment/locale/zh-cn";
+import "moment/locale/zh-cn";
 
 // 日期时间类型字段
 // value 值为GMT标准时间
@@ -13,19 +13,18 @@ export const datetime = {
     )
   },
   renderFormItem: (text: any, props: any) => {
-    const { fieldProps } = props;
+    const { fieldProps , format = "YYYY-MM-DD HH:mm:ss"} = props;
     const { defaultValue } = fieldProps;
 
     if(defaultValue){
       if(moment.isMoment(defaultValue)){
         fieldProps.defaultValue= defaultValue;
       }else{
-        fieldProps.defaultValue= moment(defaultValue, "YYYY-MM-DD HH:mm:ss");
+        fieldProps.defaultValue= moment(defaultValue, format);
       }
     }
-    console.log('props==>',props.name, props, defaultValue)
     return (
-      <FieldDatePicker text={text as string} format="YYYY-MM-DD HH:mm:ss" showTime {...props} />
+      <FieldDatePicker text={text as string} format={format}  showTime {...props} />
     )
   }
 }
