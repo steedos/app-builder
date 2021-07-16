aff6089efdb446e2abf3475f69a7538e29380255
殷亮辉 <yinlianghui@hotoa.com>
2021-07-16 21:48:03 +0800

    yml中配置defaultVaule中如果有{userId}等公式需要支持下 #117

--- packages/builder-store/src/utils/index.tsx
+++ packages/builder-store/src/utils/index.tsx
@@ -7,6 +7,38 @@ export function safeEval(js: string){
 	}
 };
 
+/**
+  把原来的默认值公式表达式转换成新的表达式语法，原来的表达式语法如下所示：
+  ==========
+  取一个字段的值：使用“{”和“}” (注意都是半角)将字段名扩起来，如：{price}
+  基于当前用户的系统变量：包括姓名、角色、部门等
+  ID: {userId}
+  姓名：{user.name}
+  所在部门（当申请人属于多个部门时，为所在主部门的全路径）： {user.organization.fullname}
+  所在部门（最底层部门名）： {user.organization.name}
+  角色名： {user.roles}
+  手机： {user.mobile}
+  固定电话： {user.work_phone}
+  职务： {user.position}
+  取当前工作区的信息
+  ID: {spaceId}
+  ===========
+  
+ * @param express 
+ */
+const getCompatibleDefaultValueExpression = (express: string)=>{
+  const reg = /^\{\w+(\.*\w+)*\}$/;//只转换{}包着的老语法，新语法是两层大括号{{}}，不运行转换
+  if(reg.test(express)){
+    if(express.indexOf("userId") > -1 || express.indexOf("spaceId") > -1 || express.indexOf("user.") > -1 || express.indexOf("now") > -1){
+      return `{${express}}`.replace("{{", "{{global.");
+    }
+    else{
+      return `{${express}}`.replace("{{", "{{formData.");
+    }
+  }
+  return express;
+}
+
 const getFieldSchema = (fieldName: any, objectConfig: any)=>{
   let fieldSchema: any = {};
   const field = objectConfig.fields[fieldName];
@@ -100,6 +132,10 @@ const getFieldSchema = (fieldName: any, objectConfig: any)=>{
   if(field._defaultValue){
     let defaultValue = safeEval(`(${field._defaultValue})`);
     fieldSchema = Object.assign({}, fieldSchema, {defaultValue});
+  }
+  else if(typeof field.defaultValue === "string"){
+    // _defaultValue属性存在说明defaultValue是一个function，不需要进行公式表达式兼容性转换
+    fieldSchema = Object.assign({}, fieldSchema, {defaultValue: getCompatibleDefaultValueExpression(field.defaultValue)});
   }
 
   if(["autonumber", "formula", "summary"].indexOf(fieldType) > -1){
